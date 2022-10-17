36ac67a0e033e78b69471aac1f341ff3cd3f6a93
pinsrq <mario.sieg.64@gmail.com>
2022-10-17 10:55:51 +0200

    Type Annoations, attempt to establish wifi connection

--- src/main.py
+++ src/main.py
@@ -2,16 +2,15 @@ import machine
 import time
 import network
 import json
-import os
 import utils
 
 # Core controller class
 class PoolControllerContext:
 
     # Constructor
-    def __init__(self, ssid, wifi_password):
+    def __init__(self, ssid: str, wifi_password: str, **wifi_config):
         print("Initializing PoolControllerContext...")
-        self.__initialize_wifi(ssid, wifi_password)
+        self.__initialize_wifi(ssid, wifi_password, **wifi_config)
         self.led = machine.Pin(2, machine.Pin.OUT)
         self.is_running = True
 
@@ -30,14 +29,22 @@ class PoolControllerContext:
         self.is_running = False
 
     # Private method - initialize WI-FI
-    def __initialize_wifi(self, ssid, wifi_password):
+    def __initialize_wifi(self, ssid: str, wifi_password: str, **wifi_config):
         assert ssid is not None and ssid != ""
         assert wifi_password is not None and wifi_password != ""
         self.wifi_module = network.WLAN(network.STA_IF)
+        self.wifi_module.config(**wifi_config)
         self.wifi_module.active(True)
         for connection in self.wifi_module.scan():
             print("Found available WI-FI connection: " + str(connection))
-        self.wifi_module.connect(ssid, wifi_password)
+            if connection[0] == ssid:
+                print("Connecting to specified WI-FI connection with SSID " + ssid)
+                self.wifi_module.connect(ssid, wifi_password)
+                while not self.wifi_module.isconnected():
+                    machine.idle()
+                print("Connection established")
+                break
+
         assert self.wifi_module.isconnected(), "Failed to connect to WI-FI"
         print("WI-FI connected: " + str(self.wifi_module.ifconfig()))
 
@@ -50,9 +57,9 @@ def main():
 
     # Load WI-FI configuration
 
-    ssid = None
-    wifi_password = None
-    config_file = "wifi_config.json"
+    ssid: str = ""
+    wifi_password: str = ""
+    config_file: str = "wifi_config.json"
 
     if not utils.does_file_exists(config_file):
         with open(config_file, "w") as f:
--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,19 @@
 import os
 
-def does_fs_entry_exists(filename):
+def does_fs_entry_exists(filename: str) -> bool:
     try:
         os.stat(filename)
         return True
     except OSError:
         return False
 
-def does_fdir_exists(filename):
+def does_fdir_exists(filename: str) -> bool:
     try:
         return (os.stat(filename)[0] & 0x4000) != 0
     except OSError:
         return False
 
-def does_file_exists(filename):
+def does_file_exists(filename: str) -> bool:
     try:
         return (os.stat(filename)[0] & 0x4000) == 0
     except OSError:
