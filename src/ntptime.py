d60577e42100ce67a668c4d582c70a2e09c3bfe7
pinsrq <mario.sieg.64@gmail.com>
2022-10-17 14:31:09 +0200

    FIxed interval tick

--- src/ntptime.py
+++ src/ntptime.py
@@ -14,25 +14,25 @@ import select
 import time
 from time import gmtime
 
+
 class NTPtime:
     # (date(2000, 1, 1) - date(1900, 1, 1)).days * 24*60*60
     # (date(1970, 1, 1) - date(1900, 1, 1)).days * 24*60*60
     NTP_DELTA = 3155673600 if gmtime(0)[0] == 2000 else 2208988800
+    SYNC_INTERVAL = 5.0
 
-    
-    host = "pool.ntp.org" # The NTP host can be configured at runtime by doing: ntptime.host = 'myhost.org'
-    hrs_offset=0 # Local time offset in hrs relative to UTC
-    
+    host = "pool.ntp.org"  # The NTP host can be configured at runtime by doing: ntptime.host = 'myhost.org'
+    hrs_offset = 0  # Local time offset in hrs relative to UTC
+
     last_timestamp = 0.0
-    sync_interval = 5 # seconds
 
     # Constructor
     # hrs_offset: Local time offset in hrs relative to UTC
-    def __init__(self, ntp_host = "pool.ntp.org",  hrs_offset=0 ):
+    def __init__(self, ntp_host="pool.ntp.org", hrs_offset=0):
         self.host = ntp_host
-        self_hrs_offset = hrs_offset
+        self.threshold_time = 0.0
         self.time()
-                 
+
     def time(self):  # Local time offset in hrs relative to UTC
         NTP_QUERY = bytearray(48)
         NTP_QUERY[0] = 0x1B
@@ -48,20 +48,23 @@ class NTPtime:
             if poller.poll(1000):  # time in milliseconds
                 msg = s.recv(48)
                 val = struct.unpack("!I", msg[40:44])[0]  # Can return 0
-                self.last_timestamp = max(val - self.NTP_DELTA + self.hrs_offset * 3600, 0)
+                x = max(val - self.NTP_DELTA + self.hrs_offset * 3600, 0)
+                self.last_timestamp = time.time()
                 return self.last_timestamp
         except OSError:
             pass  # LAN error
         finally:
             s.close()
         return 0  # Timeout or LAN error occurred
-    
-    def loop(self): # loop to sync time
-        offset = time.time() - self.last_timestamp
-        print("loop: time=" + str(time.time()))
-        print("loop: last_timestamp=" + str(self.last_timestamp))
-        print("loop: offset=" + str(offset))
-        if offset > (self.sync_interval):
-            print("NTPtime: update time *********************************************** ")
+
+    def loop(self):  # loop to sync time
+        t = time.time()
+        if self.threshold_time < t:
+            print("Syncing time from server...")
+            self.threshold_time = t + self.SYNC_INTERVAL
             self.time()
-        
+
+
+ntptime = NTPtime("pool.ntp.org", -2)
+while True:
+    ntptime.loop()
