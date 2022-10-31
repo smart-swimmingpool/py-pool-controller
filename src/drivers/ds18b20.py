d0bc4ce284eeddbd544be74c6dd83ae4719851e4
pinsrq <mario.sieg.64@gmail.com>
2022-10-31 14:43:12 +0100

    Update ds18b20.py

--- src/drivers/ds18b20.py
+++ src/drivers/ds18b20.py
@@ -1,83 +1,41 @@
-from os import path, listdir, system
-from glob import glob
+import machine, onewire, ds18x20, time
+import settings
+import uasyncio as asyncio
+from homie.device import HomieDevice, await_ready_state
+from homie.node import HomieNode
+from homie.property import HomieProperty
+from homie.constants import FLOAT
+from machine import Pin
 
-class DS18B20_Units:
-    DEGREES_CELSIUS = 0x1
-    DEGREES_FARENHEIT = 0x2
-    KELVIN = 0x3
-    FACTORS = {
-        DEGREES_CELSIUS: lambda x: x * 0.001,
-        DEGREES_FARENHEIT: lambda x: x * 0.001 * 1.8 + 32.0,
-        KELVIN: lambda x: x * 0.001 + 273.15
-    }
+class DallasSensorNode(HomieNode):
+    def __init__(self, name="Temp & Humid", pin=4, interval=60, pull=-1):
+        super().__init__(id="ds18x20", name=name, type="ds18x20")
+        print("* Init DallasSensorNode")
+        self.pin = machine.Pin(pin)
+        self.sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
+        self.interval = interval
 
-class DS18B20:
-    BASE_DIRECTORY = "/sys/bus/w1/devices"
-    SLAVE_PREFIX = "28-"
-    SLAVE_FILE = "w1_slave"
+        self.roms = ds_sensor.scan()
+        print('Found DS devices: ', roms)
+        self.p_temp = HomieProperty(
+            id="temperature",
+            name="Temperature",
+            datatype=FLOAT,
+            format="-40:80",
+            unit="°C",
+        )
+        self.add_property(self.p_temp)
+        asyncio.create_task(self.update_data())
 
-    def __init__(self, sensor_id=None, load_kernel_mods=True):
-        self._type = "DS18B20"
-        self._id = sensor_id
-        if load_kernel_mods:
-            self._load_kernel_mods()
-        self._sensor = self._get_sensor()
-
-    @property
-    def get_id(self) -> object:
-        return self._id
-
-    @property
-    def get_type(self) -> str:
-        return self._type
-
-    def does_exist(self):
-        path = self._get_sensor()
-        return path is not None and path != ""
-
-    def get_current_temp(self, unit=DS18B20_Units.DEGREES_CELSIUS) -> float:
-        ff = self._get_unit_factor(unit)
-        vv = self._get_sensor_value()
-        return ff(vv)
-
-    def get_current_temps(self, units) -> object:
-        vv = self._get_sensor_value()
-        temps = []
-        for unit in units:
-            factor = self._get_unit_factor(unit)
-            temps.append(factor(vv))
-        return temps
-
-    @classmethod
-    def get_installed_sensors(cls) -> object:
-        ret = []
-        for sensor in listdir(cls.BASE_DIRECTORY):
-            if sensor.startswith(cls.SLAVE_PREFIX):
-                ret.append(sensor[3:])
-        return ret
-
-    @classmethod
-    def get_all_sensors(cls) -> object:
-        return [DS18B20(id) for id in cls.get_installed_sensors()]
-
-    @staticmethod
-    def _get_unit_fac(self, unit):
-        return DS18B20_Units.FACTORS[unit]
-
-    def _get_sensor(self) -> str:
-        sensors = self.get_installed_sensors()
-        assert self._id in sensors
-        if not self._id and sensors:
-            self._id = sensors[0]
-        return path.join(self.BASE_DIRECTORY, self.SLAVE_PREFIX + str(self._id), self.SLAVE_FILE)
-
-    def _get_sensor_value(self) -> float:
-        with open(self._sensor, "r") as f:
-            data = f.readlines()
-        assert data[0].strip()[-3:] == "YES"
-        return float(data[1].split("=")[1])
-
-    @staticmethod
-    def _load_kernel_modules(self):
-        system("modprobe w1-gpio")
-        system("modprobe w1-therm")
+    @await_ready_state
+    async def update_data(self):
+        sensor = self.sensor
+        roms = self.roms
+        delay = self.interval * 1000
+        while True:
+            sensor.convert_temp()
+            time.sleep_ms(750)
+            for rom in roms:
+                print(rom)
+                print(sensor.read_temp(rom))
+            await asyncio.sleep_ms(delay)
